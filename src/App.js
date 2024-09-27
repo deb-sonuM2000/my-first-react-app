a0c82c1d6f5b7be6e6b11191a66e8ae1ad9280a8
debadanmohanty <debadanmohanty7735199207@gmail.com>
2024-09-27 13:04:52 +0530

    ReactProject1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import './App.css'
+import Navbar from './components/Navbar'
+import About from './components/About'
+import TextForm from './components/TextForm'
+import Alert from './components/Alert'
+import  { Routes } from 'react-router-dom'
+import {
+  BrowserRouter as Router,
+  
+  Route
+  
+} from 'react-router-dom'
 
-function App() {
+const App = () => {
+
+  const [mode, setMode] = useState('light')
+  const [alert, setAlert] = useState(null)
+
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type
+    })
+    setTimeout(() => {
+      setAlert(null)
+    }, 3000);
+  }
+
+  const toggleMode = () => {
+    if (mode === 'light') {
+      setMode('dark')
+      document.body.style.backgroundColor = "#042743"
+      showAlert('Dark mode has been enabled', 'success')
+    } else {
+      setMode('light')
+      document.body.style.backgroundColor = 'white'
+      showAlert('Light Mide has been enabled', 'success')
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Router>
+        <Navbar title="MySite2" mode={mode} toggleMode={toggleMode} aboutText="ItsAbout" />
+        <Alert alert={alert} />
+        <div className='container my-3'>
+          <Routes>
+            <Route path='/about'element={<About/>}>
+              
+            </Route>
+            <Route path='/' element={<TextForm showAlert={showAlert} heading="Enter the text Below" mode={mode} />}>
+              
+            </Route>
+
+          </Routes>
+        </div>
+      </Router>
+    </>
+
+  )
 }
 
-export default App;
+export default App
+
+
+
---
+++ src/components/TextForm.jsx
@@ -0,0 +1,46 @@
+import React,{useState} from 'react'
+
+const TextForm = (props) => {
+          const[text,setText] = useState('')
+          const handleClick=()=>{
+                    //console.log('clicked' + text)
+                    let newText = text.toUpperCase()
+                    setText(newText)
+                    props.showAlert("Converted to Uppercase", "Success")
+          }
+          const handleChange=(event)=>{
+                    setText(event.target.value)
+          }
+          const handleClick1=()=>{
+                    let newText = text.toLowerCase()
+                    setText(newText)
+                    props.showAlert("Converted to LowerCase", "Success")
+          }
+          const handleClearClick=()=>{
+                    let newText = ''
+                    setText(newText)
+                    props.showAlert("Cleared the text", "Success")
+          }
+          return (
+                    <>
+                    <div className='container my-2' style={{color:props.mode==='dark'?'white':'#042743'}} >
+                              <h1>{props.heading}</h1>
+                              <div className="mb-3">
+                                        
+                                        <textarea className="form-control" value={text} onChange={handleChange} style={{backgroundColor:props.mode==='dark'?'grey':'white'}} id="myBox" rows="8"></textarea>
+                              </div>
+                              <button className="btn btn-primary mx-1" onClick={handleClick}>Convert to UpperCase</button>
+                              <button className="btn btn-primary mx-1" onClick={handleClick1}>Convert to LowerCase</button>
+                              <button className="btn btn-primary" onClick={handleClearClick}>Clear Text</button>
+                    </div>
+                    <div className="container my-3" style={{color:props.mode==='dark'?'white':'#042743'}} >
+                              <h2>Your Text Summary</h2>
+                              <p>{text.split(" ").length} words and {text.length} characters</p>
+                              <h2>Preview</h2>
+                              <p>{text.length>0?text:"Enter something to preview"}</p>
+                    </div>
+                    </>
+          )
+}
+
+export default TextForm
